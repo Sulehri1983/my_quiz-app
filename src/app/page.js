4198d141aab5766e37cc195c7c1ae6feb5d7cb92
Sulehri1983 <aleemmustafavi@gmail.com>
2024-10-05 23:39:38 +0500

    hese both page.js and page-module.css files are edited to adjust the heading styles and the way result is expressed alongwith a symbol expressing some feelings.
    t

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react";
-import "./page-module.css";
+import  styles from "./page-module.css";
 import {questions} from "./questions";
 export default function Home(){
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -36,17 +36,17 @@ export default function Home(){
     <>
     {showResult? (
     <div className="quiz-app">
-    {score>=0.5*questions.length? (<h1>well: you have secured {score} marks out of {questions.length}</h1>):
-    <h1>good luck in future: your secured marks are {score} out of {questions.length}</h1>
+    {score>=0.8*questions.length? (<h1 className="excellent">congratulations: you have secured {score} marks out of {questions.length}</h1>):
+    score>=0.5*questions.length? (<h1 className="pass">well: your secured marks are {score} out of {questions.length}</h1>): (<h1 className="fail">good luck in future: you have secured {score} out of {questions.length}</h1>)
     }
   </div>
     ): (
       <div className="quiz-app">
       <div className="quiz-header">
-      <h1>1st DS/ML quiz</h1>
+      <h1 classname = {styles.heading}>1st DS/ML quiz</h1>
       </div>
       <div className="quiz-body">
-       <h1>{question.id}: {question.question}</h1>
+       <h1 classname = {styles['sub-heading']}>{question.id}: {question.question}</h1>
      <div className="options">
      {question.options.map((option)=> {
        return (
